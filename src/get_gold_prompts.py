7c672fd246d34282f2e76f303856a2aad98f9221
Adrian Wang <wangadr@seas.upenn.edu>
2021-05-04 19:33:08 -0400

    Add more gold quoting alternatives

--- src/get_gold_prompts.py
+++ src/get_gold_prompts.py
@@ -3,9 +3,11 @@ import csv
 import pandas as pd
 
 
-answers = pd.read_csv('../data/goldAnswers.csv', quotechar='`')
+answers = pd.read_csv('../data/goldStandard/goldAnswers.csv', quotechar='`')
 prompts = answers['Input.prompt'].rename('prompt')
-print(prompts)
-prompts.to_csv('../data/goldPromptsDoubleQuote.csv', index=False)
+replaced = prompts.str.replace(pat='"', repl="'")
+print(replaced)
+replaced.to_csv('../data/goldStandard/goldPromptsSingleQuote.csv',
+                index=False)
 # prompts.to_csv('../data/goldPrompts.csv', quotechar='`', index=False)
 # print(answers)
