9312ca2a8b8556894c61123c823e5e434f235cb8
Adrian Wang <wangadr@seas.upenn.edu>
2021-05-04 18:03:40 -0400

    Add double quoted version of gold prompts

--- src/get_gold_prompts.py
+++ src/get_gold_prompts.py
@@ -6,7 +6,6 @@ import pandas as pd
 answers = pd.read_csv('../data/goldAnswers.csv', quotechar='`')
 prompts = answers['Input.prompt'].rename('prompt')
 print(prompts)
-prompts.to_csv('../data/goldPromptsEscapeComma.csv', quoting=csv.QUOTE_NONE,
-               escapechar='\\', index=False)
+prompts.to_csv('../data/goldPromptsDoubleQuote.csv', index=False)
 # prompts.to_csv('../data/goldPrompts.csv', quotechar='`', index=False)
 # print(answers)
