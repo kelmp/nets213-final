a0eef8b991ba3ab5ecf24d277703be46453c350a
Adrian Wang <wangadr@seas.upenn.edu>
2021-05-04 17:55:17 -0400

    Add escaped comma version of gold prompts

--- src/get_gold_prompts.py
+++ src/get_gold_prompts.py
@@ -1,8 +1,12 @@
+import csv
+
 import pandas as pd
 
 
 answers = pd.read_csv('../data/goldAnswers.csv', quotechar='`')
 prompts = answers['Input.prompt'].rename('prompt')
 print(prompts)
-prompts.to_csv('../data/goldPrompts.csv', quotechar='`', index=False)
+prompts.to_csv('../data/goldPromptsEscapeComma.csv', quoting=csv.QUOTE_NONE,
+               escapechar='\\', index=False)
+# prompts.to_csv('../data/goldPrompts.csv', quotechar='`', index=False)
 # print(answers)
