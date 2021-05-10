364a5bc99d9bb9f224c2bc6c8bf761ac6719d8d5
cphalen <cphalen@seas.upenn.edu>
2021-05-10 16:53:26 -0400

    Small bug fixes and changes

--- training-data-processor/main.py
+++ training-data-processor/main.py
@@ -12,12 +12,12 @@ import csv
 def read_json(filename: str):
     data = []
     with open(filename, "r") as file:
-        csv_in = csv.reader(file, delimiter=",", quotechar="`", quoting=csv.QUOTE_ALL)
+        csv_in = csv.reader(file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_ALL)
         next(csv_in) # skip the header row
         for row in csv_in:
-            text = row[1]
+            text = row[0]
             text_split = text.split(" ")
-            for action in json.loads(row[2]):
+            for action in json.loads(row[1]):
                 entities = []
                 for key in action.keys():
                     for index in action[key]["indices"]:
@@ -28,18 +28,16 @@ def read_json(filename: str):
 
                         before_start = 0
                         for i in range(start):
-                            if start_word == text_split[i]:
-                                before_start += 1
+                            before_start += text_split[i].count(start_word)
 
                         before_end = 0
-                        for i in range(start):
-                            if end_word == text_split[i]:
-                                before_end += 1
+                        for i in range(end):
+                            before_end += text_split[i].count(end_word)
                         
-                        start_index = find_nth(text, start_word, before_start)
-                        end_index = find_nth(text, end_word, before_end) + len(end_word)
+                        start_index = find_nth(text, start_word, before_start) + 1
+                        end_index = find_nth(text, end_word, before_end) + len(end_word) + 1
                         entities.append((start_index, end_index, key))
-                data.append((text, { "entities": entities }))
+            data.append((text, { "entities": entities }))
     return data
 
 def find_nth(string: str, substring: str, n: int):
@@ -49,4 +47,4 @@ def find_nth(string: str, substring: str, n: int):
         return string.find(substring, find_nth(string, substring, n - 1) + 1)
 
 if __name__ == "__main__":
-    print(read_json("mturk.csv"))
+    print(read_json("mturk_labels.csv"))
