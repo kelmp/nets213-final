b8cccb8814bc23c5a0f050a1de93fbf7d7a793b0
Adrian Wang <wangadr@seas.upenn.edu>
2021-05-10 21:57:51 -0400

    Update filepaths of qc/aggregation stubs

--- src/aggregation2.py
+++ src/aggregation2.py
@@ -1,7 +1,7 @@
 from common import read_answers
 
 
-answer_df = read_answers('../data/sampleAggregationInput.csv', 'Answer.answer')
+answer_df = read_answers('../data/random_tests/sampleAggregationInput.csv', 'Answer.answer')
 answer_df.to_csv("../data/sampleAggregationOutput.csv", index=False, quotechar="`")
 print(answer_df)
 counts = answer_df.groupby('Answer.answer').size()
