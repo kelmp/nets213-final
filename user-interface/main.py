e4a87a5117aca8049050b1dc7f0d5b4cc8718e08
cphalen <cphalen@seas.upenn.edu>
2021-05-11 00:40:36 -0400

    Added ML model to user interface

--- user-interface/main.py
+++ user-interface/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 import json
+import spacy
 app = Flask(__name__)
+
+cls = spacy.util.get_lang_class("en")
+friends_model = spacy.load("friends_gold_model_ner")
+mturk_model = spacy.load("mturk_gold_model_ner")
 
 @app.route("/")
 def index():
@@ -9,6 +14,16 @@ def index():
 @app.route("/model", methods=['POST'])
 def run_model():
     action = request.form["action"]
-    # output = model(action)
-    output = { "action": "sample action response" }
+    model = request.form["model"]
+
+    if model == "friends":
+        doc = friends_model(action)
+    else:
+        doc = mturk_model(action)
+
+    labels = ""
+    for ent in doc.ents:
+        labels += f"{ent.label_}:{ent.text}\n"
+
+    output = { "action": labels }
     return json.dumps(output)
